130438321c09c75cc162f9db30c80318825043e5
Evelyn <huanhua_xu@ucsb.edu>
2019-11-24 16:25:08 -0800

    model update

--- Project/src/model/Account.java
+++ Project/src/model/Account.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Account {
+    private int id;
+    private float rate;
+    private String branch_name;
+    private AccountType type;
+    private String primary_owner_id;
+    private boolean closed;
+    //private float balance;
 
+    public Account(){
+
+    }
+
+    public int getID(){return id;}
+    public float getRate(){return rate;}
+    public String getBranchname(){return branch_name;}
+    public AccountType getType() { return type; }
+    public String getPrimaryOwner(){return primary_owner_id;}
+    public boolean isClosed(){return closed;}
+    //public float getBalance(){return balance;}
+
+    public void setId(int id){this.id=id;}
+    public void setRate(float rate){this.rate=rate;}
+    public void setBranch_name(String name){branch_name=name;}
+    public void setType(AccountType type){this.type=type;}
+    public void setPrimaryOwner(String customer_Id){primary_owner_id=customer_Id;}
+    public void setClosed(boolean closed){this.closed=closed;}
+    //public void setBalance(float balance){this.balance=balance;}
 }
--- Project/src/model/Transaction.java
+++ Project/src/model/Transaction.java
@@ -1,4 +1,13 @@
 package model;
 
+import javax.xml.crypto.Data;
+
 public class Transaction {
+    private String id;
+    private String type;
+    private float amount;
+    private Data time;
+    private Boolean isClosed;
+
+
 }
