801c39b3acd48311c3345dd495f6277ebf3f2c16
Evelyn <huanhua_xu@ucsb.edu>
2019-11-24 16:37:17 -0800

    model update1.0

--- Project/src/model/Account.java
+++ Project/src/model/Account.java
@@ -7,10 +7,20 @@ public class Account {
     private AccountType type;
     private String primary_owner_id;
     private boolean closed;
-    //private float balance;
+    private double balance;
 
     public Account(){
 
+    }
+
+    public Account(int id, float rate, String branch_name, AccountType type, String primary_owner_id, boolean closed, double balance){
+        this.id=id;
+        this.rate=rate;
+        this.branch_name=branch_name;
+        this.type=type;
+        this.primary_owner_id=primary_owner_id;
+        this.closed=closed;
+        this.balance=balance;
     }
 
     public int getID(){return id;}
@@ -19,7 +29,7 @@ public class Account {
     public AccountType getType() { return type; }
     public String getPrimaryOwner(){return primary_owner_id;}
     public boolean isClosed(){return closed;}
-    //public float getBalance(){return balance;}
+    public double getBalance(){return balance;}
 
     public void setId(int id){this.id=id;}
     public void setRate(float rate){this.rate=rate;}
@@ -27,5 +37,5 @@ public class Account {
     public void setType(AccountType type){this.type=type;}
     public void setPrimaryOwner(String customer_Id){primary_owner_id=customer_Id;}
     public void setClosed(boolean closed){this.closed=closed;}
-    //public void setBalance(float balance){this.balance=balance;}
+    public void setBalance(double balance){this.balance=balance;}
 }
